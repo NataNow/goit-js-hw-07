62a621887213e11f03b7a7c18bdab4a6c8932e6f
NataNow <112073825+NataNow@users.noreply.github.com>
2022-11-22 17:42:33 +0200

    gallery JS

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,77 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+function makeMarkup() {
+  let markup = "";
+  for (const item of galleryItems) {
+    const { preview = "#", original = "#", description = "#" } = item;
+    markup += `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </div>`;
+  }
+  return markup;
+}
+
+const modal = {
+  gallery: document.querySelector(".gallery"),
+  instance: {},
+
+  handleGalleryClick: function (event) {
+    event.preventDefault();
+    const { target, currentTarget } = event;
+    if (currentTarget === target) {
+      return;
+    }
+    const srcImage = target.dataset.source;
+    this.createModalImage(srcImage);
+    this.showModalImage();
+  },
+
+  createModalImage: function (srcImage) {
+    this.instance = basicLightbox.create(`<img src="${srcImage}">`, {
+      onShow: this.onShowModalImage.bind(this),
+      onClose: this.onCloseModalImage.bind(this),
+    });
+  },
+
+  onShowModalImage: function () {
+    document.addEventListener("keydown", this.handleEscapePress);
+  },
+
+  onCloseModalImage: function () {
+    document.removeEventListener("keydown", this.handleEscapePress);
+  },
+
+  handleEscapePress: function (event) {
+    if (event.code === "Escape") modal.instance.close();
+  },
+
+  renderGalleryMarkup: function (markup) {
+    this.gallery.innerHTML = markup;
+  },
+
+  showModalImage: function () {
+    this.instance.show();
+  },
+};
+
+modal.renderGalleryMarkup(makeMarkup());
+modal.gallery.addEventListener("click", modal.handleGalleryClick.bind(modal));
 console.log(galleryItems);
+
+
+
+// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. 
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки 
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this lineimport { galleryItems } from "./gallery-items.js";
 
+function createGaleryMarkup(galleryItems) {
+    const markup = galleryItems
+      .map(({ preview = "#", original = "#", description = "#" } = {}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+      })
+      .join("");
+    return markup;
+  }
+  
+  const gallery = document.querySelector(".gallery");
+  
+  const galleryMarkup = createGaleryMarkup(galleryItems);
+  
+  gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+  
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSlide: false,
+    animationSpeed: 500,
+    maxZoom: 5,
+  });
 console.log(galleryItems);
+
+
+// Зроби таку саму галерею як в першому завданні, але використовуючи бібліотеку SimpleLightbox, яка візьме на себе обробку кліків по зображеннях, відкриття і закриття модального вікна, а також гортання зображень за допомогою клавіатури.
+// Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
